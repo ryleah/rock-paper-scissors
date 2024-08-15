09ebe696842b3fca07835ba200ce43a0da918bfe
ryleah <177492644+ryleah@users.noreply.github.com>
2024-08-15 14:46:24 -0400

    Initial JavaScript for game
    All meant to be played in the browser console

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+/* let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
 for (let i = 1; i <= answer; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
@@ -10,4 +10,58 @@ for (let i = 1; i <= answer; i++) {
   } else {
     console.log(i);
   }
+}
+*/
+
+// Set the choices
+
+const choices = ["rock", "paper", "scissors"]
+let humanScore = 0
+let computerScore = 0
+
+// Function to choose rock, paper, or scissors randomly for the computer
+
+function getComputerChoice(computerChoice) {
+    return choices[Math.floor(Math.random() * 3)]
+}
+
+// Function for human choice
+
+function getHumanChoice(humanChoice) {
+    return prompt("Rock, paper, or scissors?", "rock, paper, or scissors?").toLowerCase()
+}
+
+// Play five rounds
+
+for (var roundNumber = 0; roundNumber < 5; roundNumber++) {
+
+// Get Choices
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+// Who wins, print out choices, add to score
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("human choice, computer choice: " + humanChoice + ", " +computerChoice)
+        result = "tie";
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") |
+                (humanChoice === "scissors" && computerChoice === "paper") |
+                (humanChoice === "paper" && computerChoice === "rock")) {
+                    console.log("human choice, computer choice: " + humanChoice + ", " +computerChoice);
+                    humanScore++;
+                    result = "win";
+    } else {
+        console.log("human choice, computer choice: " + humanChoice + ", " +computerChoice);
+        computerScore++;
+        result = "lost";
+    }
+
+    console.log(result + " - Computer: " + computerScore + " points. Human has: " + humanScore + " points.");
+}
+
+
+
+playRound(humanSelection, computerSelection)
 }
